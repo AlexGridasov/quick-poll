1909d1bc43a7129e88a3a9468044ec485ba8c6bb
AlexGridasov <palleev@gmail.com>
2017-06-21 11:29:21 +0300

    Added compute vote result functionality with DTOs.

---
+++ src/main/java/com/gri/alex/dto/VoteResult.java
@@ -0,0 +1,14 @@
+package com.gri.alex.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+public class VoteResult {
+
+    private int totalVotes;
+    private Collection<OptionCount> results;
+}
